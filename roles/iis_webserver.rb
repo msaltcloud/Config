48ea979bb0afa0795d0b1d3e9b22afd6f648417a
Doug Seven <altcloud@microsoft.com>
2013-05-28 01:59:46 -0700

    Updating recipes

--- roles/iis_webserver.rb
+++ roles/iis_webserver.rb
@@ -1,5 +1,8 @@
 name 'iis_webserver'
 description 'A single IIS web server'
 run_list(
-  'recipe[webpi]'
+  'recipe[webpi]',
+  'recipe[webpi::iis_recommended]',
+  'recipe[webpi::install-sql-express]',
+  'recipe[webpi::install-mvc4]'
 )
